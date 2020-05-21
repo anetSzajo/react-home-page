098d4e5ad1b3086d2ae87017350b2b678ec8a21c
Aneta Szajowska <an.szajowska@gmail.com>
2020-05-21 16:15:25 +0200

    add hamburger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import HamburgerMenu from 'react-hamburger-menu';
 
 import './header.css';
 
-function Header(props){
+class Header extends React.Component{
+    state = {
+        open: false
+    }
+    handleClick() {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+    render(){
     return(
         <div className="header">
-            <span>Homepage</span>
-            <span>App</span>
-            <span>About us</span>
+            <HamburgerMenu className="hamburger-btn"
+                isOpen={this.state.open}
+                menuClicked={this.handleClick.bind(this)}
+                width={18}
+                height={15}
+                strokeWidth={1}
+                rotate={0}
+                color='black'
+                borderRadius={0}
+                animationDuration={0.5}
+            />
+            <div className="header-list">
+                <span className="header-link" href="#">Homepage</span>
+                <span className="header-link" href="#">App</span>
+                <span className="header-link" href="#">About us</span>
+            </div>
         </div>
     )
+    }
 }
 
 export default Header;
